fde82a304233a53f4b7171830234b288c2c93afd
Tom <tomliefheid@gmail.com>
2018-08-25 00:27:02 +0200

    adding basic project structure

---
+++ mostdev-hungergames/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mostdev_hungergames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
