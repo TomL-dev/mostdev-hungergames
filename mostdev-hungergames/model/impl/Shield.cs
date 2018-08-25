b2789ea36b09fe6ad55954043b0310bf38f9ebab
Tom <tomliefheid@gmail.com>
2018-08-25 23:35:29 +0200

    adding battle items

---
+++ mostdev-hungergames/model/impl/Shield.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mostdev_hungergames.model.impl
+{
+	class Shield : BattleItem
+	{
+		public Shield()
+		{
+			this.Name = "Shield";
+			this.DefenseBonus = 15;
+		}
+	}
+}
